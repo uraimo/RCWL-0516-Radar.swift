b23321714b9683b13ed285ceba524f4347a279c5
Umberto Raimondi <me@uraimo.com>
2018-06-05 21:45:36 +0200

    SwiftyGPIO to 1.x

--- Examples/Raspi2/Package.swift
+++ Examples/Raspi2/Package.swift
@@ -2,9 +2,8 @@ import PackageDescription
 
 let package = Package(
     name: "Test",
-    targets: [],
     dependencies: [
         .Package(url: "https://github.com/uraimo/RCWL-0516-Radar.swift.git",
-                 majorVersion: 1)
+                 majorVersion: 2)
     ]
 )
--- Package.swift
+++ Package.swift
@@ -2,9 +2,8 @@ import PackageDescription
 
 let package = Package(
     name: "RCWL0516Radar",
-    targets: [],
     dependencies: [
         .Package(url: "https://github.com/uraimo/SwiftyGPIO.git",
-                 majorVersion: 0)
+                 majorVersion: 1)
     ]
 )
